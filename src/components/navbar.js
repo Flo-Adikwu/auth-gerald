73930f503ba6a366eac1a5e5ee9ada6e0ca06135
Florence <florence@esusu.org>
2022-05-12 23:41:10 +0100

    worked on profile page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,123 +6,135 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
+import { MenuList, ListItemIcon, ListItemText } from '@mui/material';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import logo from '../asset/images/logo.png'
+import logo from '../asset/images/logo.png';
+import { useNavigate } from "react-router-dom";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import GroupIcon from '@mui/icons-material/Group';
+import LogoutIcon from '@mui/icons-material/Logout';
+
 
 const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['My profile', 'Group chat', 'Logout'];
 
 const Navbar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const navigate = useNavigate()
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
-  return (
-    <AppBar  style={{background: "#fff"}}>
-      <Container maxWidth="xl">
-        <Toolbar enableGutters>
-          <img src={logo} alt="logo"/>
+    const onLogout = () => {
+        navigate("/")
+    }
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {/* {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))} */}
-            </Menu>
-          </Box>
-          
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {/* {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))} */}
-          </Box>
+    return (
+        <AppBar style={{ background: "#fff" }}>
+            <Container maxWidth="xl">
+                <Toolbar enableGutters>
+                    <img src={logo} alt="logo" style={{ width: "210px", height: "30px" }} />
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                        </Menu>
+                    </Box>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            <MenuList>
+                                <MenuItem onClick={handleCloseUserMenu}>
+                                    <ListItemIcon>
+                                        <AccountCircleIcon />
+                                    </ListItemIcon>
+                                    <ListItemText>My profile</ListItemText>
+                                </MenuItem>
+                                <MenuItem>
+                                    <ListItemIcon>
+                                        <GroupIcon />
+                                    </ListItemIcon>
+                                    <ListItemText>Group chat</ListItemText>
+                                </MenuItem><hr />
+                                <MenuItem>
+                                    <ListItemIcon style={{ color: "red" }}>
+                                        <LogoutIcon />
+                                    </ListItemIcon>
+                                    <ListItemText style={{ color: "red" }} onClick={onLogout}>Logout</ListItemText>
+                                </MenuItem>
+                            </MenuList>
+
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 };
 export default Navbar;
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,12 +1,60 @@
 import React from 'react';
 import Navbar from '../components/navbar';
+import './profile.css';
+import logo from '../asset/images/logo192.png';
+import { Button } from '@mui/material';
 
 
-const Profile =()=>{
-    return(
-        <div>
-            <Navbar/>
-        </div>
+
+
+const Profile = () => {
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <div className='heading'>
+                    <span className='personal-info'>Personal Info</span><br/>
+                    <span className='basic'>Basic info like your name and photo</span>
+                </div>
+                <div className="sub-container">
+                    <div className='partition-div'>
+                    <div>
+                        <span className='profile'>Profile</span><br/>
+                        <span className='grey'>Some info may be visible</span>
+                    </div>
+                        <Button variant="outlined" className="button">Edit</Button>
+                    </div><hr />
+                    <div className='sec'>
+                        <p className='grey'>PHOTO</p>
+                        <img src={logo} alt="img" className='left' />
+                    </div><hr />
+                    <div className='sec'>
+                        <p className='grey'>NAME</p>
+                        <p className='left'>Xanthe Neal</p>
+                    </div><hr />
+                    <div className='sec'>
+                        <p className='grey'>BIO</p>
+                        <p className='left'>I am a software developer and a big fan of devchallenges...</p>
+                    </div><hr />
+                    <div className='sec'>
+                        <p className='grey'>PHONE</p>
+                        <p className='left'>908249274292</p>
+                    </div><hr />
+                    <div className='sec'>
+                        <p className='grey'>EMAIL</p>
+                        <p className='left'>xanthe.neal@gmail.com</p>
+                    </div><hr />
+                    <div className='sec'>
+                        <p className='grey'>PASSWORD</p>
+                        <p className='left'>************</p>
+                    </div>
+                </div>
+                <div className="bottom-items">
+                    <p className='grey-text'>Created by <a href="#" className="username">username</a></p>
+                    <p className='grey-text'>devChallenges.io</p>
+                </div>
+            </div>
+        </>
     )
 }
 
